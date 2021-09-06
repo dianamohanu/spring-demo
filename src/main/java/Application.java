fe5c13b05385ed4c84ccfe92cff62c42024406f4
Mateioo7 <chismatei@gmail.com>
2021-09-06 14:55:07 +0300

    added spring annotation config

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,16 @@
+import com.evozon.config.ApplicationConfig;
 import com.evozon.model.Cart;
 import com.evozon.service.CartService;
 import com.evozon.service.impl.CartServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) {
-        CartService cartService = new CartServiceImpl();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+
+        CartService cartService = applicationContext.getBean("cartServiceImpl", CartServiceImpl.class);
 
         cartService.add("123");
 
