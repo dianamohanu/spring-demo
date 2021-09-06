23db4a9200bf40c85767c3f74fcb551ce5a0f21b
Andrei-Alexandru Mariciuc <mariciucandreii@gmail.com>
2021-09-06 15:00:31 +0300

    Java .xml Config done

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,14 @@
 import com.evozon.model.Cart;
 import com.evozon.service.CartService;
 import com.evozon.service.impl.CartServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) {
-        CartService cartService = new CartServiceImpl();
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("appContext.xml");
+        CartService cartService = appContext.getBean("cartService", CartServiceImpl.class);
 
         cartService.add("123");
 
