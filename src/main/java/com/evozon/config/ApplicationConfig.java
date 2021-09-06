d9f3db83177a9b2f2e347b9b0adf54c3e419c2e5
Gabriela-Gabor <gaborgabriela2001@yahoo.com>
2021-09-06 15:23:25 +0300

    bonus repo with annotations and service in xml

---
+++ src/main/java/com/evozon/config/ApplicationConfig.java
@@ -0,0 +1,11 @@
+package com.evozon.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ComponentScan("com.evozon")
+public class ApplicationConfig {
+
+}
