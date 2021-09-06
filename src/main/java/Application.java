bf727918abe9dfb87b1af9274ad72eedfb6c072b
Siegel Klaus <klaussiegel2@gmail.com>
2021-09-06 16:09:56 +0300

    Add xml config

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,16 +1,17 @@
-import com.evozon.model.Cart;
-import com.evozon.service.CartService;
-import com.evozon.service.impl.CartServiceImpl;
+import com.evozon.model.Product;
+import com.evozon.service.ProductService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) {
-        CartService cartService = new CartServiceImpl();
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ProductService productService = applicationContext.getBean("prodService", ProductService.class);
 
-        cartService.add("123");
+        final Product product = productService.getById("123");
 
-        final Cart cart = cartService.get();
+        System.out.println(product.getId());
 
-        System.out.println(cart.getProducts().get(0).getId());
     }
 }
--- src/main/java/com/evozon/service/impl/ProductServiceImpl.java
+++ src/main/java/com/evozon/service/impl/ProductServiceImpl.java
@@ -2,12 +2,18 @@ package com.evozon.service.impl;
 
 import com.evozon.model.Product;
 import com.evozon.repository.ProductRepository;
-import com.evozon.repository.impl.ProductRepositoryImpl;
 import com.evozon.service.ProductService;
 
 public class ProductServiceImpl implements ProductService {
 
-    private ProductRepository productRepository = new ProductRepositoryImpl();
+    private ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository ProductRepository) {
+        this.productRepository = ProductRepository;
+    }
+    public ProductServiceImpl() {
+    }
+
 
     public Product getById(String id) {
         return productRepository.getById(id);
